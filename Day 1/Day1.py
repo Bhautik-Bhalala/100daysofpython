eee5b3c25ebf3bb884e691d9a0d90bc55b37a195
Bhautik Bhalala <47077595+Bhautik-Bhalala@users.noreply.github.com>
2021-02-03 18:17:30 +0530

    day 1

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,3 +1,17 @@
+# name = "Bhautik"
+# print(name)
+#
+# name = "Bhalala"
+# print(name)
+#
+#
+# name = input("What is your name?")
+# length = len(name)
+# print(length)
+
+# print("Hello " + input("What is your name?"))
+
+
 print("Welcome to the Band Name Generator.")
 street = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
