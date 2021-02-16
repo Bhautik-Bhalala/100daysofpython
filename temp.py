a6c5cafe85b4622b93cec8d409f744db0d2655e2
Bhautik Bhalala <47077595+Bhautik-Bhalala@users.noreply.github.com>
2021-02-16 15:17:47 +0530

    temp file update

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 
 
 import os
-for i in range(7,101):
-    os.mkdir(f"Day {i}")
-    
+# for i in range(7,101):
+#     os.mkdir(f"Day {i}")
+
 for i in range(7,101):
     os.chdir(rf"C:\Users\bhautik\Desktop\100daysofpython\Day {i}")
     # os.remove(f"Day{i}")
